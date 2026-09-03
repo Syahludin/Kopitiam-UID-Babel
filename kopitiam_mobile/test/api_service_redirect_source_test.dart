952caf74d27ced67af4273611f41136d00d5af2e
Syahludin <Guswansyahludin@gmail.com>
2026-09-03 14:11:07 +0700

    test: align Apps Script redirect checks with current implementation

--- kopitiam_mobile/test/api_service_redirect_source_test.dart
+++ kopitiam_mobile/test/api_service_redirect_source_test.dart
@@ -7,14 +7,14 @@ void main() {
     final source = File('lib/services/api_service.dart').readAsStringSync();
     expect(source, contains("http.Request('POST', initialUri)"));
     expect(source, contains("http.Request('GET', contentUri)"));
-    expect(source, contains("contentUri.host != _contentHost"));
+    expect(source, contains('contentUri.host != _contentHost'));
     expect(source, isNot(contains("http.Request('POST', current)")));
   });
 
   test('credentials are not copied into the ContentService GET request', () {
     final source = File('lib/services/api_service.dart').readAsStringSync();
     final getStart = source.indexOf("final contentRequest = http.Request('GET'");
-    final getEnd = source.indexOf('final contentStream', getStart);
+    final getEnd = source.indexOf('return await http.Response.fromStream', getStart);
     expect(getStart, greaterThanOrEqualTo(0));
     expect(getEnd, greaterThan(getStart));
     final getBlock = source.substring(getStart, getEnd);
